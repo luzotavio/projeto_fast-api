a6915688f182380696ef8349f5830bf01857c114
Luiz Otávio <139489817+luzotavio@users.noreply.github.com>
2025-01-03 10:40:33 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_read_root_deve_retornar_ok_e_ola_mundo(client):  # arrange
     response = client.get('/')  # act
@@ -23,71 +25,40 @@ def test_create_user(client):
     }
 
 
-def test_get_user(client):
-    response = client.get('/user/1')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'username': 'luiz',
-        'email': 'tst@test.com',
-        'id': 1,
-    }
-
-
-def test_get_user_not_found(client):
-    response = client.get('/user/2')
-
-    assert response.json() == {'detail': 'user not found'}
-
-
 def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [{'username': 'luiz', 'email': 'tst@test.com', 'id': 1}]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    userschema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [userschema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
             'password': '123',
-            'username': 'test2',
-            'email': 'tst@test.com',
+            'username': 'Teste',
+            'email': 'teste@test.com',
             'id': 1,
         },
     )
 
     assert response.json() == {
-        'username': 'test2',
-        'email': 'tst@test.com',
+        'username': 'Teste',
+        'email': 'teste@test.com',
         'id': 1,
     }
 
 
-def test_put_user_not_found(client):
-    response = client.put(
-        '/users/2',
-        json={
-            'password': '123',
-            'username': 'test2',
-            'email': 'tst@test.com',
-            'id': 1,
-        },
-    )
-
-    assert response.json() == {'detail': 'user not found'}
-
-
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
-    assert response.json() == {'message': 'User-deleted'}
-
-
-def test_delete_user_not_found(client):
-    response = client.delete('/users/2')
-
-    assert response.json() == {'detail': 'user not found'}
+    assert response.json() == {'message': 'User deleted'}
